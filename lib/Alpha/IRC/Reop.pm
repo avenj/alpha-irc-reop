72c4757de7dbff44d3f24e5704a51c892d15a658
Jon Portnoy <avenj@cobaltirc.org>
2012-10-03 11:17:04 -0400

    Initial commit 	new file:   Build.PL 	new file:   bin/alpha-reop 	new file:   lib/Alpha/IRC/Reop.pm 	new file:   lib/Alpha/IRC/Reop/Config.pm

---
+++ lib/Alpha/IRC/Reop.pm
@@ -0,0 +1,138 @@
+package Alpha::IRC::Reop;
+
+use 5.10.1;
+use Carp;
+
+use Moo;
+use strictures 1;
+
+use POE qw/
+  Component::IRC::State
+  Component::IRC::Plugin::AutoJoin
+  Component::IRC::Plugin::Connector
+  Component::IRC::Plugin::NickReclaim
+/;
+
+use IRC::Utils qw/
+  uc_irc lc_irc
+/;
+
+use Scalar::Util 'blessed';
+
+
+
+has 'config' => (
+  required => 1,
+  is       => 'ro',
+  isa      => sub {
+    blessed $_[0]
+    and $_[0]->isa('Alpha::IRC::Config')
+    or  confess "$_[0] is not an Alpha::IRC::Config"
+  },
+);
+
+
+has 'current_ops' => (
+  ## Known ops and the last time we saw them speak
+  ## Add to this hash when opped (FIXME configurable?)
+  ## Remove and move to 'tracking' with their status modes
+  ## if they go idle
+  lazy    => 1,
+  is      => 'ro',
+  writer  => 'set_current_ops',
+  default => sub {  {}  },
+);
+
+has 'pending_ops' => (
+  lazy    => 1,
+  is      => 'ro',
+  writer  => 'set_pending_ops',
+  default => sub {  {}  },
+);
+
+
+## Create a Session when this object is instantiated.
+sub BUILD {
+  my ($self) = @_;
+
+  POE::Session->create(
+    heap => {
+      lastseen => {},
+      ## Users that may need to be reopped:
+      tracking => {},
+    },
+    object_states => [
+      $self => [ qw/
+        _start
+
+        ac_check_lastseen
+
+        irc_public
+        irc_chan_sync
+        irc_nick
+        irc_part
+        irc_quit
+      / ],
+    ],
+  );
+}
+
+
+## POE states.
+sub _start {
+  my ($kernel, $heap) = @_[KERNEL, HEAP];
+
+  my $irc = POE::Component::IRC::State->spawn(
+    ## FIXME
+  ) or die "PoCo::IRC fatal: $!";
+
+  $irc->plugin_add() ## FIXME Connector, AutoJoin, NickServID ?
+
+  $heap->{irc} = $irc;
+
+  $irc->yield(register => 'all');
+
+  $irc->yield(connect => {});
+}
+
+
+## irc_* states
+
+sub irc_public {
+  ## If this is a known op update 'lastseen'
+}
+
+sub irc_chan_sync {
+  ## Grab current operators (configurable?)
+  ## Set ac_check_lastseen timer
+}
+
+sub irc_mode {
+  ## Track added/removed status modes
+}
+
+sub irc_nick {
+  ## Track nick changes, update hashes accordingly
+  ## (Needs to check against both, potentially)
+}
+
+sub irc_part {
+  ## See if we care that we lost this user
+}
+
+sub irc_quit {
+  ## Same deal as _part
+}
+
+
+## ac_* states
+
+sub ac_check_lastseen {
+  ## Check lastseen users to see if anyone should have modes
+  ## removed (configurable delta)
+}
+
+1;
+
+## FIXME per-channel configurable re-op cmd?
+##  would allow for configurably using bots/services on certain chans
---
+++ lib/Alpha/IRC/Reop/Config.pm
@@ -0,0 +1,37 @@
+package Alpha::IRC::Reop::Config;
+
+use Carp;
+use 5.10.1;
+
+use Moo;
+use strictures 1;
+
+
+has 'nickname' => (
+  required  => 1,
+  is        => 'ro',
+  writer    => 'set_nickname',
+  predicate => 1,
+);
+
+has 'server' => (
+  required  => 1,
+  is        => 'ro',
+  writer    => 'set_server',
+  predicate => 1,
+);
+
+has 'channels' => (
+  required  => 1,
+  is        => 'ro',
+  writer    => 'set_channels',
+  predicate => 1,
+);
+
+has 'status_modes' => (
+  is        => 'ro',
+  writer    => 'set_status_modes',
+  predicate => 1,
+);
+
+1;
